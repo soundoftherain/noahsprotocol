2de8c5bbb566cc2b985a9e1ae74a2fb886676612
soundoftherain <m47612815@gmail.com>
2025-10-03 12:44:33 +0200

    feat: minimal React + Tailwind starter files for homepage MVP

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+import HeroSection from './components/HeroSection';
+
+const App = () => {
+  return (
+    <div className="App">
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
