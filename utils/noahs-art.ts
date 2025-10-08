c252d4696fc2ee703ea8b368cb0abb894c15903a
soundoftherain <m47612815@gmail.com>
2025-10-08 18:00:53 +0200

    Delete utils directory

--- utils/noahs-art.ts
+++
@@ -1,6 +0,0 @@
-export const episodes = [
-  { id: 1, title: "Genesis Wave", phase: "Dawn" },
-  { id: 2, title: "Fractal Current", phase: "Dusk" },
-  { id: 3, title: "Chromatic Rift", phase: "Dawn" },
-  { id: 4, title: "Aurora Assembly", phase: "Dusk" },
-];
